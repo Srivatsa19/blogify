d2b589a715a62cfeb2ec00c6d2d18d6cec48dec5
Srivatsa19 <srivatsapotdar19@gmail.com>
2024-07-05 18:44:49 +0530

    Fixed the prerendering on vercel in write/page.jsx

--- src/app/write/page.jsx
+++ src/app/write/page.jsx
@@ -25,42 +25,41 @@ const Write = () => {
   const [catSlug, setCatSlug] = useState("");
 
   useEffect(() => {
-    const upload = () => {
+    if (typeof window !== "undefined") {
+      const upload = () => {
+        const uniqueName = new Date().getTime() + file.name;
+        const storage = getStorage();
+        const storageRef = ref(storage, uniqueName);
+        const uploadTask = uploadBytesResumable(storageRef, file);
 
-      // Firebase docs
-
-      const uniqueName = new Date().getTime + file.name
-
-      const storageRef = ref(storage, uniqueName);
-
-      const uploadTask = uploadBytesResumable(storageRef, file);
-
-      uploadTask.on('state_changed',
-        (snapshot) => {
-          const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
-          console.log('Upload is ' + progress + '% done');
-          switch (snapshot.state) {
-            case 'paused':
-              console.log('Upload is paused');
-              break;
-            case 'running':
-              console.log('Upload is running');
-              break;
+        uploadTask.on(
+          "state_changed",
+          (snapshot) => {
+            const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+            console.log("Upload is " + progress + "% done");
+            switch (snapshot.state) {
+              case "paused":
+                console.log("Upload is paused");
+                break;
+              case "running":
+                console.log("Upload is running");
+                break;
+            }
+          },
+          (error) => {
+            console.error("Error uploading file:", error);
+          },
+          () => {
+            getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+              console.log("File available at", downloadURL);
+              setMedia(downloadURL);
+            });
           }
-        },
-        (error) => {
-        },
-        () => {
-          getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
-            // console.log('File available at', downloadURL);
-            setMedia(downloadURL);
-          });
-        }
-      );
-
-    };
-    file && upload();
-  }, [file])
+        );
+      };
+      file && upload();
+    }
+  }, [file]);
 
   if (status === "loading") {
     return <div className={styles.loading}>...</div>
