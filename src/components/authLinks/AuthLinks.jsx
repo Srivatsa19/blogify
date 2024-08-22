645064a411b20d5bbd7c50ff8d2e7cae41d8b19e
Srivatsa19 <srivatsapotdar19@gmail.com>
2024-08-22 16:35:34 +0530

    Corrected the logout link

--- src/components/authLinks/AuthLinks.jsx
+++ src/components/authLinks/AuthLinks.jsx
@@ -32,7 +32,7 @@ const AuthLinks = () => {
             ) : (
               <>
                 <Link href="/write">Write</Link>
-                <span className={styles.link}>Logout</span>
+                <span className={styles.link} onClick={signOut}>Logout</span>
               </>
             )}
         </div>
